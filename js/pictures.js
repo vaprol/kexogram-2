ff53761e4953635ba3104243751ce11176b07206
Konstantin Pivnov <vaprol@gmail.com>
2019-06-19 23:04:26 +0300

    Добавил валидацию хэштегов

--- js/pictures.js
+++ js/pictures.js
@@ -137,6 +137,54 @@ commentInput.addEventListener('blur', function () {
     document.addEventListener('keydown', onEscHideUploadOverlay);
 });
 
+var uploadForm = document.querySelector('#upload-select-image');
+var uploadFormSubmitButton = document.querySelector('#upload-submit');
+var validateHashtags = function () {
+    var isHashtagValid = true;
+    if (hashtagInput.value !== '') {
+        var hashtagsArray = hashtagInput.value.split(' ');
+        if (hashtagsArray.length > 5) {
+            hashtagInput.setCustomValidity('Поле должно содержать не более 5 хэштегов разделенных пробелами');
+            isHashtagValid = false;
+        } else {
+            for (var i = 0; i < hashtagsArray.length; i++) {
+                if (isHashtagValid) {
+                    var hashtag = hashtagsArray[i];
+                    if (hashtag.length > 20 ||
+                        hashtag.length === 1 ||
+                        hashtag[0] !== '#') {
+                        hashtagInput.setCustomValidity('Хэштег должен начинаться с решетки и быть длиной от 2 до 20 знаков.');
+                        isHashtagValid = false;
+                        break;
+                    } else {
+                        for (var j = 1; j < hashtag.length; j++) {
+                            if (hashtag[j] === ',' ||
+                                hashtag[j] === '#') {
+                                hashtagInput.setCustomValidity('Хэштеги должны разделяться пробелами');
+                                isHashtagValid = false;
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    break;
+                }
+
+            }
+        }
+    }
+    return isHashtagValid;
+};
+onUploadFormSubmit = function (evt) {
+    // debugger;
+    evt.preventDefault();
+    if (validateHashtags()) {
+        uploadForm.submit();
+    } else {
+        hashtagInput.reportValidity();
+    }
+};
+uploadFormSubmitButton.addEventListener('click', onUploadFormSubmit);
 
 var onSaturationPinDrop = function (evt) {
     // console.log(evt);
